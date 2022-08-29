68a88f61e5aba8a1388a182ade7e4b162f2aa623
bharat.soni <bharat.soni@ihsmarkit.com>
2022-08-29 15:10:42 +0530

    Adding more programs

--- src/com/practice/queue/ImplementQueueUsingStack.java
+++ src/com/practice/queue/ImplementQueueUsingStack.java
@@ -2,21 +2,37 @@ package com.practice.queue;
 
 import java.util.Stack;
 
-public class ImplementQueueUsingStack {
-    Stack<Integer> s1 = new Stack<>();
-    Stack<Integer> s2 = new Stack<>();
+/*
+This approach affects the dequeue operation’s time complexity instead of the enqueue operation.
+
+To enqueue an item into the queue, push the item into the first stack.
+To dequeue an item from the queue, move elements from the first stack to the second stack if it is not empty,
+and return the top item from the second stack.
+ */
+public class ImplementQueueUsingStack<T> {
+    Stack<T> s1 = new Stack<>();
+    Stack<T> s2 = new Stack<>();
 
     //O(1)
-    void push(int data) {
+    void push(T data) {
+        // push item into the first stack
         s1.push(data);
     }
 
     //O(N)
-    public int pop() {
+    public T pop() {
+        // if both stacks are empty
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Underflow!!");
+            System.exit(0);
+        }
+        //move elements from s1 to s2
         while (!s1.isEmpty()) {
             s2.push(s1.pop());
         }
-        int res = s2.pop();
+        //keep pointer to top element to be returned as result
+        T res = s2.pop();
+        //move back elements from s2 to s1
         while ((!s2.isEmpty())) {
             s1.push(s2.pop());
         }
@@ -24,14 +40,28 @@ public class ImplementQueueUsingStack {
     }
 
     //O(N)
-    public int peek() {
+    public T peek() {
         while (!s1.isEmpty()) {
             s2.push(s1.pop());
         }
-        int res = s2.peek();
+        T res = s2.peek();
         while ((!s2.isEmpty())) {
             s1.push(s2.pop());
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] keys = {1, 2, 3, 4, 5};
+
+        // insert the above keys into the stack
+        ImplementQueueUsingStack<Integer> q = new ImplementQueueUsingStack<>();
+        for (int key : keys) {
+            q.push(key);
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            System.out.println(q.pop());
+        }
     }
 }
---
+++ src/com/practice/stack/ImplementStackUsingQueues.java
@@ -0,0 +1,74 @@
+package com.practice.stack;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+// Implement stack using two queues
+/*
+ In @ImplementStackUsingQueue.java Note that the elements are exchanged between the queue twice for every push operation.
+This can impact performance if push operations are frequent. Here’s an alternative approach that affects the pop operation’s time complexity instead of the push operation.
+
+To push an item into the stack, enqueue the item to the first queue.
+To pop an item from the stack, move all elements from the first queue to the second queue except the last element,
+and then return the last element after moving all elements back to the first queue.
+ */
+class ImplementStackUsingQueues<T> {
+    Queue<T> q1, q2;
+
+    // Constructor
+    public ImplementStackUsingQueues() {
+        q1 = new ArrayDeque<>();
+        q2 = new ArrayDeque<>();
+    }
+
+    // Insert an item into the stack
+    void add(T data) {
+        // Push the given item into the first queue
+        q1.add(data);
+    }
+
+    // Remove the top item from the stack
+    public T poll() {
+        // if the first queue is empty
+        if (q1.isEmpty()) {
+            System.out.println("Stack Underflow!!");
+            System.exit(0);
+        }
+
+        // Move all elements except last from the first queue to the second queue
+        T front = null;
+        while (!q1.isEmpty()) {
+            if (q1.size() == 1) {
+                front = q1.peek();
+            } else {
+                q2.add(q1.peek());
+            }
+
+            q1.poll();
+        }
+
+        // Return the last element after moving all elements back to
+        // the first queue
+        while (!q2.isEmpty()) {
+            q1.add(q2.peek());
+            q2.poll();
+        }
+
+        return front;
+    }
+
+    public static void main(String[] args) {
+        int[] keys = {1, 2, 3, 4, 5};
+
+        // insert the above keys into the stack
+        ImplementStackUsingQueues<Integer> s = new ImplementStackUsingQueues<>();
+        for (int key : keys) {
+            s.add(key);
+        }
+
+        for (int i = 0; i < keys.length; i++) {
+            System.out.println(s.poll());
+        }
+    }
+}
+
